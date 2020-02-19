ebb01cc01839c1648c6aab3c41dd2ef3f68c709d
Kevin Wu <Kevin.Wu@GCCYJPYRJ1WK.local>
2020-02-19 14:03:46 -0500

    update

---
+++ JSwarmup.js
@@ -0,0 +1,37 @@
+sum1 = 0;
+sum2 = 0;
+sum3 = 0;
+
+
+
+list = [4,7,9,10,11];
+
+//write a loop to calculate the sum of the list
+for(int i=0; i<list.length;i++) {
+	sum1 += list[i];
+}
+console.log(sum1);
+
+//write a loop to find the sum of the even values
+for(int i=0;i<list.length;i++) {
+	if(list[i]%2===0) {
+		sum2 += list[i];
+	}
+}
+console.log(sum2);
+
+//write a loop to find the sum of values that are greater than 5
+
+for(int i=0;i<list.length();i++) {
+	if(list[i]>5) {
+		sum3 += list[i];
+	}
+}
+print(sum3);
+
+//write a loop that finds the sum of the digits (11= 2)
+
+
+
+
+
